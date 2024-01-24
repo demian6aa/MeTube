eba0d361d39f507b23d05b1cc7e15a0f05842273
Demian A <146421652+demian6aa@users.noreply.github.com>
2024-01-24 18:56:49 +0100

    LeftNavBar  (1)
    Left navigation bar main first iteration

---
+++ JS/LeftNav.js
@@ -0,0 +1,42 @@
+const Nav = document.getElementById('leftNavID');
+const NavBack = document.querySelector('.leftNav-background');
+const menuButtons = document.querySelectorAll('.menu-button');
+// const Nav = document.querySelector('.leftNav');
+let navBarOpen = false;
+
+NavBack.style
+
+NavBack.addEventListener('click', hideNav )
+
+
+menuButtons.forEach(function(menuButtons) 
+{
+  menuButtons.addEventListener('click',alternateNav);
+});
+
+
+function hideNav(){
+  Nav.style.transform = 'translate3D(-100%, 0,0)';
+  NavBack.style.opacity = '0'
+  NavBack.style.pointerEvents = 'none';
+  navBarOpen = false;
+}
+
+function showNav(){
+  Nav.style.transform = 'translate3D(0, 0,0)';
+  NavBack.style.opacity = '100'
+  NavBack.style.pointerEvents = 'all';
+  navBarOpen = true;
+}
+
+//Simple function to check if the nav is open or not for the MenuButtons funcionality
+function alternateNav(){
+  console.log(navBarOpen);
+  if(navBarOpen == true){
+    hideNav();
+    console.log('entra en hide');
+  }else{
+    console.log('entra en show');
+    showNav();
+  }
+}
